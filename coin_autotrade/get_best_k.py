47aa0319bed25f66b8c62a1ac6317c66a6eaf44f
prepepper <47060573+prepepper@users.noreply.github.com>
2021-05-13 23:57:20 +0900

    Update get_best_k.py

--- coin_autotrade/get_best_k.py
+++ coin_autotrade/get_best_k.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_ror(k=0.5):
-    df = pyupbit.get_ohlcv("KRW-BTC")
+    df = pyupbit.get_ohlcv("KRW-BTC", count=7)  # ex) count=7  7일간 데이터 가져오기
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
