0131150e2a5b1cc720155af7a4da3626471ec48a
prepepper <47060573+prepepper@users.noreply.github.com>
2021-05-13 23:06:25 +0900

    Add files via upload

---
+++ coin_autotrade/get_best_k.py
@@ -0,0 +1,27 @@
+import pyupbit
+import numpy as np
+
+
+def get_ror(k=0.5):
+    df = pyupbit.get_ohlcv("KRW-BTC")
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    # 수수료까지 계산
+    # fee = 0.0032
+    # df['ror'] = np.where(df['high'] > df['target'],
+    #                      df['close'] / df['target'] - fee,
+    #                      1)
+    
+    # 수수료 미포함 k값 구하기
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+
+for k in np.arange(0.1, 1.0, 0.1):
+    ror = get_ror(k)
+    print("%.1f %f" % (k, ror))
